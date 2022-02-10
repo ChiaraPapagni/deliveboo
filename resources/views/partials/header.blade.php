dce8dbe65265d6435014267146dc3c793b925e45
DenuccioDiego <diegodenuccio95@gmail.com>
2022-02-11 00:39:29 +0100

    Fixed position logo and list links in hamburger

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,12 +2,16 @@
 <div class="nav">
   <input type="checkbox" id="nav-check">
 
-  <div class="nav-header">
+  {{-- <div class="nav-header">
     <div class="nav-title">
       JoGeek
     </div>
+  </div> --}}
+
+  <div class="logo">
+    
   </div>
-  
+
   <div class="nav-btn">
     <label for="nav-check">
       <span></span>
@@ -15,13 +19,23 @@
       <span></span>
     </label>
   </div>
-  
-  <div class="nav-links">
-    <a href="//github.io/jo_geek" target="_blank">Github</a>
-    <a href="http://stackoverflow.com/users/4084003/" target="_blank">Stackoverflow</a>
-    <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">LinkedIn</a>
-    <a href="https://codepen.io/jo_Geek/" target="_blank">Codepen</a>
-    <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">JsFiddle</a>
+
+  <div class="container nav-links d-flex justify-content-between w-100">
+
+    <div>
+      <a href="//github.io/jo_geek" target="_blank">Ristoranti</a>
+      <a href="http://stackoverflow.com/users/4084003/" target="_blank">La cucina che vorrei</a>
+    </div>
+   
+
+    <div>
+      <a href="//github.io/jo_geek" target="_blank">Login</a>
+      <a href="http://stackoverflow.com/users/4084003/" target="_blank">Registrazione Ristoratore</a>
+    </div>
+    
+    
   </div>
+  
+  
 </div>
 
