b48d1619875f34036206c82e6eaf1dfb8e9fc529
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2022-02-16 16:36:10 +0100

    FIX UU

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -17,6 +17,7 @@ class CategorySeeder extends Seeder
         $categories = ['italiano', 'cinese', 'giapponese', 'messicano', 'pesce', 'carne', 'pizza'];
         $cat_images = ['cat_italiano.jpg', 'cat_cinese.jpg', 'cat_giapponese.jpg', 'cat_messicano.jpg', 'cat_pesce.jpg', 'cat_carne.jpg', 'cat_pizza.jpg'];
 
+
         foreach ($categories as $id => $category) {
             $cat = new Category();
             $cat->name = $category;
