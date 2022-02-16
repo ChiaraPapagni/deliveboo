fdff8893912f00f5fbae68533ef10b3d6e792994
Emanuele117 <emanuele.pica7@gmail.com>
2022-02-16 16:08:28 +0100

    modifiche css

--- resources/views/guest/restaurant/show.blade.php
+++ resources/views/guest/restaurant/show.blade.php
@@ -17,26 +17,35 @@
         </div>
     </div>
 
+    <div class="top-pics mt-5">
+        <div class="line"></div>
+        <div class="text">Top Picks</div>
+        <div class="line"></div>
+    </div>
+
     {{-- griglia cibo --}}
-    <div class="container mt-5">
+    <div class="container mt-3">
+        <p class="paragraph ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, quasi eveniet quibusdam
+            totam repellat nemo,
+            reiciendis iusto officia animi placeat asperiores!</p>
         <div class="row row-panino">
             <div class="col-lg-6 md-2 sm-2">
-                <div class="yellow">
+                <div class="img-container">
                     <img src="http://127.0.0.1:8000/img-prova/panino1.png" alt="" class="img">
                 </div>
             </div>
             <div class="col-lg-6 md-2 sm-2">
-                <div class="yellow">
+                <div class="img-container">
                     <img src="http://127.0.0.1:8000/img-prova/panino1.png" alt="" class="img">
                 </div>
             </div>
             <div class="col-lg-6 md-2 sm-2">
-                <div class="yellow">
+                <div class="img-container">
                     <img src="http://127.0.0.1:8000/img-prova/panino1.png" alt="" class="img">
                 </div>
             </div>
             <div class="col-lg-6 md-2 sm-2">
-                <div class="yellow">
+                <div class="img-container">
                     <img src="http://127.0.0.1:8000/img-prova/panino1.png" alt="" class="img">
                 </div>
             </div>
@@ -44,9 +53,13 @@
         </div>
     </div>
 
+    <div class="top-pics mt-5">
+        <div class="line"></div>
+        <div class="text">Choose your meal</div>
+        <div class="line"></div>
+    </div>
 
-    <div class="container mt-5 testo">
-        <h1>Scegli il tuo pasto</h1>
+    <div class="container mt-4 testo">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus doloribus laboriosam esse provident sed ipsam
             voluptas.</p>
     </div>
@@ -55,25 +68,25 @@
     <div class="container mt-5">
         <div class="row">
             <div class="col-lg-3">
-                <img src="http://127.0.0.1:8000/img-prova/panino1.png" alt="" class="img">
+                <img src="http://127.0.0.1:8000/img-prova/panino1.png" alt="" class="img-pasto">
                 <h3>Panino prova</h3>
                 <p>10$</p>
                 <p>descrizione</p>
             </div>
             <div class="col-lg-3">
-                <img src="http://127.0.0.1:8000/img-prova/panino1.png" alt="" class="img">
+                <img src="http://127.0.0.1:8000/img-prova/panino1.png" alt="" class="img-pasto">
                 <h3>Panino prova</h3>
                 <p>10$</p>
                 <p>descrizione</p>
             </div>
             <div class="col-lg-3">
-                <img src="http://127.0.0.1:8000/img-prova/panino1.png" alt="" class="img">
+                <img src="http://127.0.0.1:8000/img-prova/panino1.png" alt="" class="img-pasto">
                 <h3>Panino prova</h3>
                 <p>10$</p>
                 <p>descrizione</p>
             </div>
             <div class="col-lg-3">
-                <img src="http://127.0.0.1:8000/img-prova/panino1.png" alt="" class="img">
+                <img src="http://127.0.0.1:8000/img-prova/panino1.png" alt="" class="img-pasto">
                 <h3>Panino prova</h3>
                 <p>10$</p>
                 <p>descrizione</p>
