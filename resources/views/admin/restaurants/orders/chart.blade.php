17e2fdb17943204c91988ad3103859eccfda04fb
Chiara <chiaraiors@gmail.com>
2022-02-24 18:26:39 +0100

    FEAT scroll

--- resources/views/admin/restaurants/orders/chart.blade.php
+++ resources/views/admin/restaurants/orders/chart.blade.php
@@ -1,12 +1,21 @@
 @extends('layouts.admin')
 
-@section('content')
-    <div>
-        <canvas id="canvas" width="240px" height="135px"></canvas>
-    </div>
+@section('personalCss')
+    <link rel="stylesheet" href="{{ asset('css/app.css') }} ">
+    <link href="{{ asset('css/restaurantEdit.css') }}" rel="stylesheet">
+@endsection
 
-    <div>
-        <canvas id="canvas2" width="240px" height="135px"></canvas>
+@section('content')
+    <div class="scroll">
+        <div class="container mt-5">
+            <div>
+                <canvas id="canvas" width="240px" height="135px"></canvas>
+            </div>
+
+            <div class="pt-5">
+                <canvas id="canvas2" width="240px" height="135px"></canvas>
+            </div>
+        </div>
     </div>
 @endsection
 
@@ -17,13 +26,13 @@
     var data_y = JSON.parse('{!! json_encode($monthOrders) !!}')
     var data_x1 = JSON.parse('{!! json_encode($months) !!}')
     var data_y2 = JSON.parse('{!! json_encode($amountOrders) !!}')
-
+    Chart.defaults.global.defaultFontColor = "#fff";
     console.log(data_y, data_x1, data_y2[0].sums);
     var barChartData = {
         labels: data_x1,
         datasets: [{
             label: 'Total Orders',
-            backgroundColor: "pink",
+            backgroundColor: "#ca1d1f87",
             data: data_y,
         }]
     };
@@ -31,7 +40,7 @@
         labels: data_x1,
         datasets: [{
             label: 'Amount orders',
-            backgroundColor: "pink",
+            backgroundColor: "#ca1d1f87",
             data: data_y2,
         }]
     };
@@ -44,15 +53,17 @@
             options: {
                 elements: {
                     rectangle: {
-                        borderWidth: 1,
-                        borderColor: '#c1c1c1',
+                        borderWidth: 0,
+                        borderColor: '#fff',
                         borderSkipped: 'bottom',
                     }
                 },
                 responsive: true,
                 title: {
                     display: true,
-                    text: 'Numero Ordini per anno/mese'
+                    text: 'Numero Ordini per anno/mese',
+                    color: '#ffffff',
+                    fontSize: 18,
                 },
                 scales: {
                     yAxes: [{
@@ -71,7 +82,7 @@
             options: {
                 elements: {
                     rectangle: {
-                        borderWidth: 1,
+                        borderWidth: 0,
                         borderColor: '#c1c1c1',
                         borderSkipped: 'bottom',
                     }
@@ -79,7 +90,9 @@
                 responsive: true,
                 title: {
                     display: true,
-                    text: 'Fatturato per mese/anno'
+                    text: 'Fatturato per mese/anno',
+                    color: '#ffffff',
+                    fontSize: 18,
                 },
                 scales: {
                     yAxes: [{
