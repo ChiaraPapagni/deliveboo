6585145bd9157ea0fefe9f6c2ce990e4e3f20f66
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2022-02-24 12:42:06 +0100

    FEAT Made singleton for braintree keys

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace App\Providers;
 
-
+use Braintree;
 use Braintree\Gateway;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,10 +29,17 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(Gateway::class, function ($app) {
             return new Gateway(
                 [
-                    'environment' => env('BRAINTREE_ENV'),
-                    'merchantId' => env('BRAINTREE_MERCHANT_ID'),
-                    'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
-                    'privateKey' => env('BRAINTREE_PRIVATE_KEY')
+
+                    'environment' => getenv('BRAINTREE_ENV'),
+                    'merchantId' => getenv('BRAINTREE_MERCHANT_ID'),
+                    'publicKey' => getenv('BRAINTREE_PUBLIC_KEY'),
+                    'privateKey' => getenv('BRAINTREE_PRIVATE_KEY')
+
+                    // CHANGE WITH YOUR STATIC DATA
+                    // 'environment' => 'sandbox',
+                    // 'merchantId' => 'kvntg6fcgx6pn4v9',
+                    // 'publicKey' => 'w55j5pb4c9pvgmvw',
+                    // 'privateKey' => 'fbbe3822890f8cd804bbe4bb9c8ce459'
                 ]
             );
         });
